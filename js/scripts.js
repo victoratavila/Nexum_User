2d982097876d43b4996d49a9f9344961461a878d
= <victoratavila@hotmail.com>
2020-11-21 14:03:54 -0300

    Backend integration

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+    // Cadastrar imigrante
+    
     $('#btnSendData').click(() => {
 
           const firstName = $('#firstName').val();
@@ -57,3 +59,48 @@
       function restart(){
         location.reload();
       }
+
+
+  $('#btnSendHelperData').click(() => {
+    const firstName = $('#firstName').val();
+    const lastName = $('#lastName').val();
+    const email = $('#email').val();
+    const currentCountry = $('#currentCountry').val();
+    const currentCity = $('#currentCity').val();
+    const businessman = $('#businessman').val();
+    const jobHelp = $('#jobHelp').val();
+    const habitationHelp = $('#habitationHelp').val();
+    const studyingHelp = $('#studyingHelp').val();
+    const helpDescription = $('#helpDescription').val();
+
+    const helperData = {
+      firstName, lastName, email, currentCity, currentCountry, businessman, jobHelp, habitationHelp, studyingHelp, helpDescription
+    }
+
+    var i = 0; var size = Object.keys(helperData);
+    var dataside = 0;
+
+    for(var info in helperData){
+      // console.log(info);
+      if(helperData[info] == undefined || helperData[info] == null || helperData[info] == ''){
+        dataside;
+      } else {
+        dataside += 1;
+      }
+    }
+
+    if(dataside == 10){
+      axios({
+      method: 'post',
+      url: 'http://localhost:3000/helper',
+      data: helperData
+      }).then(() => {
+          $('#exampleModal').modal('show');
+      }).catch(err => {
+          $('#exampleModal2').modal('show');
+      });
+    } else {
+          $('#exampleModal2').modal('show');
+    }
+    
+  })
